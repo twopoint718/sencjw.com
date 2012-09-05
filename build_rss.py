ac257db6f27a23f9d821298801e9c49c1249aef9
Chris Wilson <christopher.j.wilson@gmail.com>
2012-09-05 12:47:48 -0500

    WIP: render markdown for rss feed

--- build_rss.py
+++ build_rss.py
@@ -2,6 +2,7 @@ import yaml
 import hashlib
 import time
 import sys
+from subprocess import *
 
 yaml_file = "posts.yaml"
 date_fmt= "%a, %d %b %Y 00:00:00 +0000"
@@ -35,6 +36,7 @@ def build_items(outfile):
         posts = yaml.load(f.read())
         for post in posts:
             date = post['date']
+            post['postbody'] = run_markdown(post['postbody'])
             post['ymd_date'] = date.strftime(ymd_fmt)
             post['date_fmt'] = date.strftime(date_fmt)
             h = hashlib.sha1()
@@ -42,6 +44,14 @@ def build_items(outfile):
             post['guid'] = h.hexdigest()
             out = item_template % post
             outfile.write(out.encode("utf-8"))
+
+def run_markdown(txt):
+    p = Popen("pandoc", shell=True, bufsize=512, stdin=PIPE, stdout=PIPE, close_fds=True)
+    p.stdin.write(unicode(txt))
+    p.stdin.close()
+    output_html = p.stdout.read()
+    p.stdout.close()
+    return output_html
 
 def build_header(outfile):
     info = dict()
